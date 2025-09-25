b1d52666d6bfcddfe1a98e60371696dbecafbacb
FilipeVBF <filipevbf@gmail.com>
2025-09-24 21:33:33 -0300

    feat(auth): integrar sign-in e sign-up com a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "./index.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 
-import { router } from "./routes";
-import { Button } from "./components/ui/button";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
+import { router } from "./routes";
 
 export function App() {
   return (
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Toaster richColors />
-      <RouterProvider router={router} />;
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />;
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,13 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { toast } from "sonner";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "@/api/sign-in";
 
 const signInForm = z.object({
   email: z.email(),
@@ -14,15 +16,25 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const [searchParams] = useSearchParams();
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>();
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get("email") ?? "",
+    },
+  });
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  });
 
   async function handleSignIn(data: SignInForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await authenticate({ email: data.email });
 
       console.log(data);
 
