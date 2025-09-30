98cef195c4ad2310c442d8ad90642ff23bb2b975
FilipeVBF <filipevbf@gmail.com>
2025-09-30 17:47:56 -0300

    feat(api): integração com endpoints de perfil e restaurante
    - Criadas funções para consumir a API:
      - getManagedRestaurant
      - getProfile
      - signOut
      - updateProfile
    - Carregados dados da API no account-menu
    - Criado dialog com informações do perfil da loja
    - Adicionada funcionalidade de atualização de dados da loja

---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,150 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "./ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import {
+  getManagedRestaurant,
+  type GetManagedRestaurantResponse,
+} from "@/api/get-managed-restaurant";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { updateProfile } from "@/api/update-profile";
+import { toast } from "sonner";
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().nullable(),
+});
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
+
+export function StoreProfileDialog() {
+  const queryClient = useQueryClient();
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurant,
+    staleTime: Infinity,
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      "managed-restaurant",
+    ]);
+
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ["managed-restaurant"],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      );
+    }
+
+    return { cached };
+  }
+
+  const { mutateAsync: updateProfileFn, isPending } = useMutation({
+    mutationFn: updateProfile,
+    onMutate({ name, description }) {
+      const { cached } = updateManagedRestaurantCache({ name, description });
+
+      return { previousProfile: cached };
+    },
+    onError(_, __, context) {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile);
+      }
+    },
+  });
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description,
+      });
+
+      toast.success("Perfil atualizado com sucesso!");
+    } catch {
+      toast.error("Falha ao atualizar o perfil, tente novamente!");
+    }
+  }
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informaçõess do seu estabelecimento visíveis ao seu
+          cliente.
+        </DialogDescription>
+      </DialogHeader>
+
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Nome
+            </Label>
+            <Input id="name" className="col-span-3" {...register("name")} />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-right">
+              Descrição
+            </Label>
+            <Textarea
+              id="description"
+              className="col-span-3"
+              {...register("description")}
+            />
+          </div>
+        </div>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="ghost" className="cursor-pointer">
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button
+            type="submit"
+            variant="success"
+            disabled={isPending}
+            className="cursor-pointer"
+          >
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  );
+}
